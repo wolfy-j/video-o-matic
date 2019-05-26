6767fe4ebc7c9ebf47c58821cd02500eeb0b40a6
Wolfy-J <wolfy.jd@gmail.com>
2019-05-26 13:28:07 +0300

    cs

--- app.php
+++ app.php
@@ -7,19 +7,12 @@
  */
 declare(strict_types=1);
 
-use Spiral\Debug\Dumper;
-
 mb_internal_encoding('UTF-8');
 error_reporting(E_ALL | E_STRICT);
 ini_set('display_errors', 'stderr');
 
 //Composer
 require __DIR__ . '/vendor/autoload.php';
-
-function dumprr($value)
-{
-    dump($value, Dumper::ERROR_LOG);
-}
 
 //Initiating shared container, bindings, directories and etc
 $app = \App\App::init(['root' => __DIR__]);
